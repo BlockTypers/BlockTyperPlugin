9dcd9c2d98692f45fab0662612fc8a4e83d5dd3d
BuildTools <unconfigured@null.spigotmc.org>
2017-02-05 18:43:01 -0500

    removed furnace craft listener

--- src/main/java/com/blocktyper/v1_1_8/recipes/RecipeCraftingListener.java
+++ src/main/java/com/blocktyper/v1_1_8/recipes/RecipeCraftingListener.java
@@ -15,7 +15,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.inventory.FurnaceSmeltEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
