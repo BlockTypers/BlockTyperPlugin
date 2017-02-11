22b05aea77485eb739ceae4f4f0de7acc135bc3a
BuildTools <unconfigured@null.spigotmc.org>
2017-02-10 21:30:26 -0500

    updated to version 1.2.1.  Added VillagerHelper and cooldown method to PlayerHelper

--- src/main/java/com/blocktyper/v1_2_0/IBlockTyperUtility.java
+++
@@ -1,7 +0,0 @@
-package com.blocktyper.v1_2_0;
-
-public interface IBlockTyperUtility {
-
-	void init(IBlockTyperPlugin plugin);
-
-}
---
+++ src/main/java/com/blocktyper/v1_2_1/IBlockTyperUtility.java
@@ -0,0 +1,7 @@
+package com.blocktyper.v1_2_1;
+
+public interface IBlockTyperUtility {
+
+	void init(IBlockTyperPlugin plugin);
+
+}
